382b1ad8ffab87f83fd5b738c4afdf1bb5607459
Flumpiey <15123171+flumpiey@users.noreply.github.com>
2021-07-09 15:40:34 +0200

    Fixed wp request limit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,11 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         url: process.env.WPGRAPHQL_URL || `https://www.connold.co.za/graphql`,
+        schema: {
+          perPage: 20, // currently set to 100
+          requestConcurrency: 5, // currently set to 15
+          previewRequestConcurrency: 2, // currently set to 5
+        },
       },
     },
     `gatsby-plugin-postcss`,
